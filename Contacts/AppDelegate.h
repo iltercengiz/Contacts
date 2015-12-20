3a1054e4e7f3083b3f1187f03769bb370ffa4392
Ilter Cengiz <iltercengiz@yahoo.com>
2015-12-20 15:42:44 +0200

    Initial Commit

---
+++ Contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Ilter Cengiz on 20/12/2015.
+//  Copyright © 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by Ilter Cengiz on 20/12/2015.
+//  Copyright © 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
